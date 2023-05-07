376f7599a4c992b9971eb9976736bd45105c2cf8
Abu Hasnat <hasnat.tushar@gmail.com>
2023-05-07 15:07:36 -0500

    Add project files.

---
+++ CAlculator_Project_abu_hasnat/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace CAlculator_Project_abu_hasnat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberOne = new System.Windows.Forms.Button();
+            this.numberTwo = new System.Windows.Forms.Button();
+            this.NumberThree = new System.Windows.Forms.Button();
+            this.numberFour = new System.Windows.Forms.Button();
+            this.numberFive = new System.Windows.Forms.Button();
+            this.numberSix = new System.Windows.Forms.Button();
+            this.numberSeven = new System.Windows.Forms.Button();
+            this.numberEight = new System.Windows.Forms.Button();
+            this.numberNine = new System.Windows.Forms.Button();
+            this.numberZero = new System.Windows.Forms.Button();
+            this.deciamlButton = new System.Windows.Forms.Button();
+            this.equalButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.resultWindow = new System.Windows.Forms.Label();
+            this.operatorWindow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // numberOne
+            // 
+            this.numberOne.AccessibleDescription = "Button For Number One";
+            this.numberOne.AccessibleName = "Number One";
+            this.numberOne.Location = new System.Drawing.Point(159, 151);
+            this.numberOne.Name = "numberOne";
+            this.numberOne.Size = new System.Drawing.Size(94, 29);
+            this.numberOne.TabIndex = 0;
+            this.numberOne.Text = "1";
+            this.numberOne.UseVisualStyleBackColor = true;
+            // 
+            // numberTwo
+            // 
+            this.numberTwo.AccessibleDescription = "Button For Number Two";
+            this.numberTwo.AccessibleName = "Number Two";
+            this.numberTwo.Location = new System.Drawing.Point(259, 151);
+            this.numberTwo.Name = "numberTwo";
+            this.numberTwo.Size = new System.Drawing.Size(94, 29);
+            this.numberTwo.TabIndex = 1;
+            this.numberTwo.Text = "2";
+            this.numberTwo.UseVisualStyleBackColor = true;
+            // 
+            // NumberThree
+            // 
+            this.NumberThree.AccessibleDescription = "Button For Number Three";
+            this.NumberThree.AccessibleName = "Number Three";
+            this.NumberThree.Location = new System.Drawing.Point(359, 151);
+            this.NumberThree.Name = "NumberThree";
+            this.NumberThree.Size = new System.Drawing.Size(94, 29);
+            this.NumberThree.TabIndex = 2;
+            this.NumberThree.Text = "3";
+            this.NumberThree.UseVisualStyleBackColor = true;
+            // 
+            // numberFour
+            // 
+            this.numberFour.AccessibleDescription = "Button For Number Four";
+            this.numberFour.AccessibleName = "Number Four";
+            this.numberFour.Location = new System.Drawing.Point(159, 186);
+            this.numberFour.Name = "numberFour";
+            this.numberFour.Size = new System.Drawing.Size(94, 29);
+            this.numberFour.TabIndex = 3;
+            this.numberFour.Text = "4";
+            this.numberFour.UseVisualStyleBackColor = true;
+            // 
+            // numberFive
+            // 
+            this.numberFive.AccessibleDescription = "Button For Number Five";
+            this.numberFive.AccessibleName = "Number Five";
+            this.numberFive.Location = new System.Drawing.Point(259, 186);
+            this.numberFive.Name = "numberFive";
+            this.numberFive.Size = new System.Drawing.Size(94, 29);
+            this.numberFive.TabIndex = 4;
+            this.numberFive.Text = "5";
+            this.numberFive.UseVisualStyleBackColor = true;
+            // 
+            // numberSix
+            // 
+            this.numberSix.AccessibleDescription = "Button For Number Six";
+            this.numberSix.AccessibleName = "Number Six";
+            this.numberSix.Location = new System.Drawing.Point(359, 186);
+            this.numberSix.Name = "numberSix";
+            this.numberSix.Size = new System.Drawing.Size(94, 29);
+            this.numberSix.TabIndex = 5;
+            this.numberSix.Text = "6";
+            this.numberSix.UseVisualStyleBackColor = true;
+            // 
+            // numberSeven
+            // 
+            this.numberSeven.AccessibleDescription = "Button For Number Seven";
+            this.numberSeven.AccessibleName = "Number Seven";
+            this.numberSeven.Location = new System.Drawing.Point(159, 221);
+            this.numberSeven.Name = "numberSeven";
+            this.numberSeven.Size = new System.Drawing.Size(94, 29);
+            this.numberSeven.TabIndex = 6;
+            this.numberSeven.Text = "7";
+            this.numberSeven.UseVisualStyleBackColor = true;
+            // 
+            // numberEight
+            // 
+            this.numberEight.AccessibleDescription = "Button For Number Eight";
+            this.numberEight.AccessibleName = "Number Eight";
+            this.numberEight.Location = new System.Drawing.Point(259, 221);
+            this.numberEight.Name = "numberEight";
+            this.numberEight.Size = new System.Drawing.Size(94, 29);
+            this.numberEight.TabIndex = 7;
+            this.numberEight.Text = "8";
+            this.numberEight.UseVisualStyleBackColor = true;
+            // 
+            // numberNine
+            // 
+            this.numberNine.AccessibleDescription = "Button For Number Nine";
+            this.numberNine.AccessibleName = "Number Nine";
+            this.numberNine.Location = new System.Drawing.Point(359, 221);
+            this.numberNine.Name = "numberNine";
+            this.numberNine.Size = new System.Drawing.Size(94, 29);
+            this.numberNine.TabIndex = 8;
+            this.numberNine.Text = "9";
+            this.numberNine.UseVisualStyleBackColor = true;
+            // 
+            // numberZero
+            // 
+            this.numberZero.AccessibleDescription = "Button For Number Zero";
+            this.numberZero.AccessibleName = "Number Zero";
+            this.numberZero.Location = new System.Drawing.Point(159, 256);
+            this.numberZero.Name = "numberZero";
+            this.numberZero.Size = new System.Drawing.Size(94, 29);
+            this.numberZero.TabIndex = 9;
+            this.numberZero.Text = "0";
+            this.numberZero.UseVisualStyleBackColor = true;
+            // 
+            // deciamlButton
+            // 
+            this.deciamlButton.AccessibleDescription = "Button  For Decimal";
+            this.deciamlButton.AccessibleName = "Decimal Button";
+            this.deciamlButton.Location = new System.Drawing.Point(259, 256);
+            this.deciamlButton.Name = "deciamlButton";
+            this.deciamlButton.Size = new System.Drawing.Size(94, 29);
+            this.deciamlButton.TabIndex = 10;
+            this.deciamlButton.Text = ".";
+            this.deciamlButton.UseVisualStyleBackColor = true;
+            // 
+            // equalButton
+            // 
+            this.equalButton.AccessibleDescription = "Button For Equal Sign";
+            this.equalButton.AccessibleName = "Equal Sign Button";
+            this.equalButton.Location = new System.Drawing.Point(359, 256);
+            this.equalButton.Name = "equalButton";
+            this.equalButton.Size = new System.Drawing.Size(94, 29);
+            this.equalButton.TabIndex = 11;
+            this.equalButton.Text = "=";
+            this.equalButton.UseVisualStyleBackColor = true;
+            // 
+            // plusButton
+            // 
+            this.plusButton.AccessibleDescription = "Button For Plus Sign";
+            this.plusButton.AccessibleName = "Plus Sign Button";
+            this.plusButton.Location = new System.Drawing.Point(459, 151);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(94, 29);
+            this.plusButton.TabIndex = 12;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            // 
+            // minusButton
+            // 
+            this.minusButton.AccessibleDescription = "Button For Minus Sign";
+            this.minusButton.AccessibleName = "Minus Sign Button";
+            this.minusButton.Location = new System.Drawing.Point(459, 186);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(94, 29);
+            this.minusButton.TabIndex = 13;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.AccessibleDescription = "Sign For multiply Sign";
+            this.multiplyButton.AccessibleName = "Multiply Sign Button";
+            this.multiplyButton.Location = new System.Drawing.Point(459, 221);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(94, 29);
+            this.multiplyButton.TabIndex = 14;
+            this.multiplyButton.Text = "*";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // divideButton
+            // 
+            this.divideButton.AccessibleDescription = "Button For Divide Sign";
+            this.divideButton.AccessibleName = "Divide Sign Button";
+            this.divideButton.Location = new System.Drawing.Point(459, 256);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(94, 29);
+            this.divideButton.TabIndex = 15;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.AccessibleDescription = "Button For Clear Window";
+            this.clearButton.AccessibleName = "Clear Sign Button";
+            this.clearButton.Location = new System.Drawing.Point(459, 116);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(94, 29);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // resultWindow
+            // 
+            this.resultWindow.AccessibleDescription = "Window For Results";
+            this.resultWindow.AccessibleName = "Result Window";
+            this.resultWindow.AutoSize = true;
+            this.resultWindow.Location = new System.Drawing.Point(181, 128);
+            this.resultWindow.Name = "resultWindow";
+            this.resultWindow.Size = new System.Drawing.Size(0, 20);
+            this.resultWindow.TabIndex = 17;
+            // 
+            // operatorWindow
+            // 
+            this.operatorWindow.AccessibleDescription = "Window For Operator Sign";
+            this.operatorWindow.AccessibleName = "Operatoe Sign Window";
+            this.operatorWindow.AutoSize = true;
+            this.operatorWindow.Location = new System.Drawing.Point(377, 128);
+            this.operatorWindow.Name = "operatorWindow";
+            this.operatorWindow.Size = new System.Drawing.Size(0, 20);
+            this.operatorWindow.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.operatorWindow);
+            this.Controls.Add(this.resultWindow);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.equalButton);
+            this.Controls.Add(this.deciamlButton);
+            this.Controls.Add(this.numberZero);
+            this.Controls.Add(this.numberNine);
+            this.Controls.Add(this.numberEight);
+            this.Controls.Add(this.numberSeven);
+            this.Controls.Add(this.numberSix);
+            this.Controls.Add(this.numberFive);
+            this.Controls.Add(this.numberFour);
+            this.Controls.Add(this.NumberThree);
+            this.Controls.Add(this.numberTwo);
+            this.Controls.Add(this.numberOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button numberOne;
+        private Button numberTwo;
+        private Button NumberThree;
+        private Button numberFour;
+        private Button numberFive;
+        private Button numberSix;
+        private Button numberSeven;
+        private Button numberEight;
+        private Button numberNine;
+        private Button numberZero;
+        private Button deciamlButton;
+        private Button equalButton;
+        private Button plusButton;
+        private Button minusButton;
+        private Button multiplyButton;
+        private Button divideButton;
+        private Button clearButton;
+        private Label resultWindow;
+        private Label operatorWindow;
+    }
+}
---
+++ CAlculator_Project_abu_hasnat/Form1.cs
@@ -0,0 +1,10 @@
+namespace CAlculator_Project_abu_hasnat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
